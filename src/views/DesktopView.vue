61c1b435c8a44aa9934e57b972abab89a60d1999
V3D1 <maciej.kick@gmail.com>
2024-12-09 10:30:30 +0100

    add some dependencies, config and wallpaper

---
+++ src/views/DesktopView.vue
@@ -0,0 +1,12 @@
+<template>
+    <main class="wallpaper bg-cover bg-center bg-no-repeat flex justify-center items-center h-screen">
+        <div class="bg-sky-500 h-40 w-40 text-white flex justify-center items-center">WindowFolio</div>
+    </main>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+.wallpaper {
+  background-image: url(@/assets/images/Lightwallpaper.webp);
+}
+</style>
+
